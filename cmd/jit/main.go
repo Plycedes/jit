99ce337f93959120d38806a05c7d042772d8d8a4
Plycedes <abhaypratapyadav340@gmail.com>
2025-10-28 16:41:29 +0530

    Adding jit init command

--- cmd/jit/main.go
+++ cmd/jit/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/plycedes/jit/internal/repo"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "jit",
+		Short: "Jit - a Git implementation in Go",
+		Long:  "Jit is a naive attempt made of a junior dev of implementing core Git features using Go",
+	}
+
+	rootCmd.AddCommand(initCmd())
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func initCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "init",
+		Short: "Initialize a new, empty Jit repository",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return repo.InitRepo(".")
+		},
+	}
+}
